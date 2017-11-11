49ceb1a750ca32b29273f4e53f31e83024637e6f
Alex Fey <alex.loofet@gmail.com>
2017-11-11 17:28:37 +0100

    React helmet SEO setup for SSR

--- src/client/pages/UsersListPage.js
+++ src/client/pages/UsersListPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { fetchUsers } from '../actions'
+import { Helmet } from 'react-helmet'
 
 class UsersList extends Component {
   componentDidMount() {
@@ -11,9 +12,19 @@ class UsersList extends Component {
       return <li key={user.id}>{user.name}</li>
     })
   }
+
+  head() {
+    return (
+      <Helmet>
+        <title>Users App</title>
+        <meta property="og:title" content="Users App" />
+      </Helmet>
+    )
+  }
   render() {
     return (
       <div>
+        {this.head()}
         users
         <ul>{this.renderUsers()}</ul>
       </div>
